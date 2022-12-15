d22224a8f0d95de2272a4c99cd2bc019ab54211f
AgustinRow <agus.diaz.11@gmail.com>
2022-12-15 14:52:59 -0300

    fix: config

--- src/deliveries/express/server.js
+++ src/deliveries/express/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { json, urlencoded } from 'body-parser';
 import morgan from 'morgan';
 import cors from 'cors';
-import { config } from 'dotenv/types';
+import { port } from 'config';
 
 const app = express();
 
@@ -10,8 +10,6 @@ app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan('dev'));
-
-const port = 3000;
 
 export const start = () => {
   app.listen(port, () => {
